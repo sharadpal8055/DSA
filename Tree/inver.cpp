103d9a39cd08ccf1d836e42c534e74b80045460d
sharadpal8055 <sharadpal230255@gmail.com>
2026-04-17 01:05:53 +0530

    invertree

---
+++ Tree/inver.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void invert(TreeNode* root) {
+        if (root == NULL) return;
+
+        // swap children
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        invert(root->left);
+        invert(root->right);
+    }
+
+    TreeNode* invertTree(TreeNode* root) {
+        invert(root);
+        return root;
+    }
+};
